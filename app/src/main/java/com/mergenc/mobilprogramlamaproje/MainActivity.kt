3abf1171c3700e9da0fffeb346411a56d1db0112
tahaaltuntasdev <m.tahaaltuntas@gmail.com>
2022-12-12 19:13:29 +0300

    Taha baba

--- app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
+++ app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mergenc.mobilprogramlamaproje
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//Taha baba
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
