663211208946f50466dedd81c8e0bd65d7349384
Mehmet Emin Ergenc <mergencdev@gmail.com>
2022-12-14 14:35:59 +0300

    -Change launcher activity -Add button in main screen for service activity

--- app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
+++ app/src/main/java/com/mergenc/mobilprogramlamaproje/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.mergenc.mobilprogramlamaproje
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mergenc.mobilprogramlamaproje.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnServiceExample.setOnClickListener {
+            startActivity(Intent(this, ServiceExampleActivity::class.java))
+        }
     }
 }
